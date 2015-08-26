7a0ed0d189fe12f44254d2ae62aea4cddf432bff
Gerson Alexander Pardo Gamez <jeral17@gmail.com>
2015-08-25 21:20:05 -0500

    added entities template and forms template

---
+++ languages/base/parser/type.js
@@ -0,0 +1,113 @@
+var util = require('util'),
+    keysParser = require('../parser/type'),
+    schemaParser = require('../../base/parser/schema');
+
+exports.foreignType = function (property, schemas) {
+
+    var fallbackType = 'INT(11)';
+
+    var propertyReferenced = property.ref;
+    if (schemas.hasOwnProperty(propertyReferenced)) {
+
+        var schemaReferenced = schemaParser.parse(schemas[propertyReferenced]);
+        if (schemaReferenced.properties) {
+            Object.keys(schemaReferenced.properties).forEach(function (name) {
+
+                var property = schemaReferenced.properties[name];
+                if (property.primary) {
+                    propertyReferenced = property;
+                    return true;
+                }
+
+            });
+        }
+
+        if (!propertyReferenced) {
+            return fallbackType;
+        }
+
+        return exports.type(propertyReferenced, schemas);
+    } else {
+        return fallbackType;
+    }
+
+};
+
+exports.length = function (property) {
+    return property.maximum ? property.maximum.toString().length : 255;
+};
+
+
+exports.type = function (property, schemas) {
+
+    var type = '';
+
+    if (property.enum) {
+        var values = [];
+        property.enum.forEach(function (value) {
+            values.push(util.format('\'%s\'', value));
+        });
+
+        type = util.format('ENUM (%s)', values.toString());
+    }
+
+    if (type) {
+        return type;
+    }
+
+    var length = exports.length(property);
+
+    switch (property.type) {
+        case 'string':
+            type = 'string';
+            break;
+        case 'number':
+            type = 'integer';
+            break;
+        case 'integer':
+            type = 'integer';
+            break;
+        case 'boolean':
+            type = 'boolean';
+            break;
+        case 'object':
+            type = exports.foreignType(property, schemas);
+            break;
+        case 'array':
+            //TODO agregar soporte para arrays
+            type = 'VARCHAR(250)';
+            break;
+        case 'null':
+            type = 'CHAR(1)';
+            break;
+        case 'any':
+            type = 'string';
+            break;
+
+        //FIXME estos no son estandares JSON SCHEMA
+        case 'text':
+            type = 'string';
+            break;
+        case 'date':
+            type = 'date';
+            break;
+        case 'datetime':
+            type = 'datetime';
+            break;
+        case 'time':
+            type = 'datetime';
+            break;
+        case 'timestamp':
+            type = 'timestamp';
+            break;
+        case 'file':
+            type = 'string';
+            break;
+        default:
+            type = 'string';
+            break;
+    }
+
+
+    return type;
+};
