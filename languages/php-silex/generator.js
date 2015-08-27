8c24cbb763f62346823ebf4d45948b18194feedd
Gerson Alexander Pardo Gamez <jeral17@gmail.com>
2015-08-27 16:36:49 -0500

    updated readme

--- languages/php-silex/generator.js
+++ languages/php-silex/generator.js
@@ -35,7 +35,6 @@ module.exports = {
                 Controller: {},
                 Entity: {},
                 Form: {
-                    DataTransformer: {},
                     Extensions: {
                         Doctrine: {
                             Bridge: {}
